032f6b1849cce4858d47c6d88874428f9ee633f7
michalriha <michal.riha@hotmail.com>
2013-11-29 18:11:21 +0100

    typo

--- UniMocker/src/cz/zcu/kiv/bp/unimocker/Mocker.java
+++ UniMocker/src/cz/zcu/kiv/bp/unimocker/Mocker.java
@@ -83,7 +83,7 @@ public class Mocker implements IMocker, BundleContextAware
 	}
 
     /**
-     * Tries to load given described from the given bundle.
+     * Tries to load described classes from the given bundle.
      * @param bundle which should be probed
      * @param classesToFind array of class names to find
      * @return array of loaded classes
