42ef3217e5e9b96e32f08980d3951b14befb79bd
michalriha <michal.riha@hotmail.com>
2013-11-29 17:04:39 +0100

    added: preview of xml schema part for custom code injection

--- Namespaces/src/cz/zcu/kiv/bp/namespaces/UniMocker.java
+++ Namespaces/src/cz/zcu/kiv/bp/namespaces/UniMocker.java
@@ -8,7 +8,7 @@ public class UniMocker
 {
 	public static final String SCENARIO_PREFIX = "um";
 	public static final String SCENARIO_URI = "http://www.kiv.zcu.cz/component-testing/mocker";
-	public static final String SCENARIO_SCHEMA = "https://raw.github.com/michalriha/kivComponentTesting/extract_common_datatypes/UniMockerBindings/schema/unimocker.xsd";
+	public static final String SCENARIO_SCHEMA = "https://raw.github.com/michalriha/kivComponentTesting/custom_code_injection/UniMockerBindings/schema/unimocker.xsd";
 	public static final String SCENARIO_SCHEMA_LOCATION = SCENARIO_URI + " " + SCENARIO_SCHEMA;
 	
 	private UniMocker() { }
