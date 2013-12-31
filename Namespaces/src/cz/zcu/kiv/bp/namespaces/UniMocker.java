c167c04a67bffbc55d74824e7cf68461f32567d5
michalriha <michal.riha@hotmail.com>
2013-12-31 11:18:15 +0100

    added: schema files  - Xml schemas now containt unique constrains on listing elements.
    changed: samples
     - Samples mocker_scenario.xml and mocker_scneario.xml use master branch
    for schema location.

--- Namespaces/src/cz/zcu/kiv/bp/namespaces/UniMocker.java
+++ Namespaces/src/cz/zcu/kiv/bp/namespaces/UniMocker.java
@@ -8,7 +8,7 @@ public class UniMocker
 {
 	public static final String SCENARIO_PREFIX = "um";
 	public static final String SCENARIO_URI = "http://www.kiv.zcu.cz/component-testing/mocker";
-	public static final String SCENARIO_SCHEMA = "https://raw.github.com/michalriha/kivComponentTesting/CommonDataTypes_custom_types/UniMockerBindings/schema/unimocker.xsd";
+	public static final String SCENARIO_SCHEMA = "https://raw.github.com/michalriha/kivComponentTesting/master/UniMockerBindings/schema/unimocker.xsd";
 	public static final String SCENARIO_SCHEMA_LOCATION = SCENARIO_URI + " " + SCENARIO_SCHEMA;
 	
 	private UniMocker() { }
